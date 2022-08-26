753175a5bea8e9ab4b7b574d84671d6ee43c7eb2
yekkuluri <rajasekharareddy@gmail.com>
2022-08-26 21:58:11 +0530

    adding project

---
+++ src/main/java/com/javatpoint/ActuatorAutoConfigurationExampleApplication.java
@@ -0,0 +1,11 @@
+package com.javatpoint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ActuatorAutoConfigurationExampleApplication 
+{
+public static void main(String[] args) 
+{
+SpringApplication.run(com.javatpoint.ActuatorAutoConfigurationExampleApplication.class, args);
+}
+}
